34b20d1d927bdcc7204717544e02c6905b78d51b
Al-amin Rahim <alamin102410@gmail.com>
2025-12-30 12:08:26 +0600

    first commit

---
+++ contracts/Staking.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
